367a28bff5dfc21fdfad7e7efdc2a18b74a189ec
naiitder <naiitder@gmail.com>
2025-06-15 23:49:52 +0200

    LadderAnimations

--- Michi-Mechanika/Assets/Scripts/Tile.cs
+++ Michi-Mechanika/Assets/Scripts/Tile.cs
@@ -5,11 +5,15 @@ public class Tile : MonoBehaviour
 {
     [HideInInspector] public Vector3 position;
     public Tile[] connectedTiles;
-    [SerializeField] private float positionY = 4;
+    [SerializeField] private Transform pivotPosition;
+    [SerializeField] public enum Type { Floor, Roof }
+    
+    [SerializeField] public Type tileType = Type.Floor;
+    
 
     private void Awake()
     {
-        position = transform.position;
-        position.y = positionY;
+        position = pivotPosition.position;
     }
+    
 }
